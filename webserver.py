fc31fe4854908da2be524a7973d4ea3f88baa081
Chopsticki <44534051+nxm3@users.noreply.github.com>
2020-08-17 14:19:12 -0500

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,19 +1,23 @@
-import http.server
-import socketserver
-
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
-announce = "Test"
-
-
-httpd = socketserver.TCPServer(("localhost", PORT), Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
-
-# announcements stuff below (e.g; announcement definitions)
-
-def announce():
-    f = open("demofile2.txt", "a")
-    f.write("<b>" + announce + "</b>")
-    f.close()
-    print("done!")
+<html>
+    <head>
+        <title>Your Choice</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+	    <div class="navbar"><img src="logo.png">PyServer &bull; A Python webserver! &bull; <a href="https://github.com/nxm3/PyServer">GitHub Repository</a></div>
+		
+        <h1>PyServer 1.2</h1>
+        <p>If you're reading this, your site is working!</p>
+		<p> Updates: </p>
+		<ul>
+		<li>Announcements! <br>
+		- Add "announce()" to your webserver file!
+		</li>
+		<li>Panic mode! <br>
+		- Messed up something serious? Start new with "panic()"!
+		<br>
+		- <strong> YOU ONLY USE THIS IF EVERYTHING IS SERIOUSLY MESSED UP. </strong>
+		</li>
+		
+    </body>
+</html>
